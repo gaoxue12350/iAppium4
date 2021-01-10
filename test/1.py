5f2cce934ad298f7425add27f044677733eee28a
gaoxue <973140551@qq.com>
2021-01-10 22:54:56 +0800

    app3

---
+++ test/1.py
@@ -0,0 +1,4 @@
+import os
+
+print(f'{os.path.abspath(os.curdir)}')
+print(f'{os.path.abspath(os.curdir)}/../app/ContactManager.apk')
--- test/cm_test.py
+++ test/cm_test.py
@@ -19,12 +19,15 @@ class IAppium(unittest.TestCase):
         desired_caps['appActivity'] = config['desired_caps']['appActivity']
         desired_caps['automationName'] = config['desired_caps']['automationName']
         desired_caps['noReset'] = config['desired_caps']['noReset']
-        desired_caps['app'] = f'{os.path.abspath(os.curdir)}/../app/ContactManager.apk'
+        desired_caps['app'] = f'{os.path.abspath(os.curdir)}/app/ContactManager.apk'
 
         self.driver = webdriver.Remote(appium_server_url, desired_caps)
 
     def tearDown(self):
         self.driver.quit()
+
+    def test_location(self):
+        print(f'{os.path.abspath(os.curdir)}/../app/ContactManager.apk')
 
     def test_contact(self):
         """  """
